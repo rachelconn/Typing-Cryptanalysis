f65f54ae649906406bcc036d83160c31ad7423b1
Rachel Conn <rconn478@gmail.com>
2022-11-05 16:21:20 -0500

    Create script to generate labeled typing dataset

---
+++ record_training_data.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+import keyboard
+import os
+import pyaudio
+import time
+import wave
+
+# Create label folder
+audio_folder = 'audio'
+label_folder = 'labels'
+os.makedirs(audio_folder, exist_ok=True)
+os.makedirs(label_folder, exist_ok=True)
+
+# Record audio
+p = pyaudio.PyAudio()
+nchannels = 1
+sampwidth = p.get_sample_size(pyaudio.paInt16)
+framerate = 44100
+frames_per_buffer = 1024
+start_time = datetime.now().timestamp()
+wf = wave.open(os.path.join(audio_folder, f'{start_time:.6f}.wav'), 'wb')
+wf.setnchannels(nchannels)
+wf.setsampwidth(sampwidth)
+wf.setframerate(framerate)
+def record_audio(recorded_data, *_):
+    wf.writeframes(recorded_data)
+    return (recorded_data, pyaudio.paContinue)
+audio_stream = p.open(
+    format=pyaudio.paInt16,
+    channels=nchannels,
+    rate=framerate,
+    frames_per_buffer=frames_per_buffer,
+    input=True,
+    stream_callback=record_audio,
+)
+audio_stream.start_stream()
+
+# Record all keyboard presses with a timestamp
+filename = datetime.now().strftime('%Y%m%d%H%M%S')
+keys_pressed = set()
+with open(os.path.join(label_folder, filename), 'w') as label_file:
+    def handle_event(e: keyboard.KeyboardEvent):
+        if e.event_type == keyboard.KEY_UP:
+            keys_pressed.discard(e.name)
+        # Don't write to file unless this is a new keypress
+        elif e.event_type == keyboard.KEY_DOWN and e.name not in keys_pressed:
+            keys_pressed.add(e.name)
+            normalized_time = e.time - start_time
+            label_file.write(f'{normalized_time} {e.name}\n')
+
+    keyboard.hook(handle_event)
+
+    # Idle until manually stopped, recording audio and key events
+    while True:
+        time.sleep(1)
