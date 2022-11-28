2ee54c3832ecff95c19ca505cd4ccec4e2def69d
Rachel Conn <rconn478@gmail.com>
2022-11-27 19:10:03 -0600

    Create utility for creating vocabfiles

---
+++ create_vocabfile.py
@@ -0,0 +1,23 @@
+"""
+Creates vocab.json, a json object mapping vocab tokens to their class indices.
+Wav2Vec2CTCTokenizer takes in this vocab file as an argument to determine what tokens are in the model's vocabulary.
+"""
+import json
+import os
+
+output_filename = 'vocab.json'
+label_file_directory = 'labels'
+
+# Collect keystrokes from all label files
+keystrokes = set()
+for filename in os.listdir(label_file_directory):
+    path = os.path.join(label_file_directory, filename)
+    with open(path, 'r') as label_file:
+        for line in label_file.readlines():
+            _timestamp, keystroke = line.rstrip().split(' ', maxsplit=1)
+            keystrokes.add(keystroke)
+
+# Write out the collected keystrokes to vocab.json
+vocab_data = {token: i for i, token in enumerate(sorted(keystrokes))}
+with open(output_filename, 'w') as output_file:
+    output_file.write(json.dumps(vocab_data, indent=4))
